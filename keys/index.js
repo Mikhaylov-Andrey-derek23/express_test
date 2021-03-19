b6df552061ec73dc4bdc02f205fd8f3df827b6ca
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2021-03-19 16:13:01 +0300

    fix keys

--- keys/index.js
+++ keys/index.js
@@ -2,5 +2,5 @@ module.exports = {
     MONGOBD_URL : 'mongodb+srv://derek:1234@cluster0.6ccds.mongodb.net/shop',
     SESSION_SECRET : 'some secret value',
     SENDGRIP_API_KEY :"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",
-    BASE_URL : "http://localhost:3000" 
+    BASE_URL : "https://express-test1-derek.herokuapp.com" 
 }
